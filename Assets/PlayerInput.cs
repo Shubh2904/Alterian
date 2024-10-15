aaa96eee7a709e9da99739a5701d060ba1fa816f
Shubh <86512551+Shubh2904@users.noreply.github.com>
2024-10-15 18:30:13 +0530

    Added Player, Input Scripts

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] InputData inputData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(Input.GetKeyDown(inputData.up))
+
+        // else(Input.GetKeyDown(inputData.down))
+        
+        // if(Input.GetKeyDown(inputData.left))
+
+        // else(Input.GetKeyDown(inputData.right))
+
+        
+    }
+}
---
+++ Assets/Scriptable Objs/InputData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Asset DB/InputData")]
+public class InputData : ScriptableObject
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    public KeyCode fire1;
+    public KeyCode fire2;
+    public KeyCode fire1Alt;
+    public KeyCode fire2Alt;
+
+    public KeyCode interact;
+    public KeyCode escape;
+    
+}
