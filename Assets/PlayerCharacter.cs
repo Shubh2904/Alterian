2eb23571ae42ca83b8f4ec0f14670b5ee4187766
Shubh <86512551+Shubh2904@users.noreply.github.com>
2024-10-15 22:56:42 +0530

    Walking Anims Script

--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerCharacter : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Animator animator;
+    [HideInInspector] public string direction;
+    [HideInInspector] public string action;
+
+    [HideInInspector] public Vector2 moveDir = Vector2.zero;
+    [SerializeField] float speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,28 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.Play($"{action} {direction}");
     }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
+    }
+
+    public void setMoveY(float y)
+    {
+        moveDir = new Vector2(moveDir.x, y);
+    }
+
+    public void setMoveX(float x)
+    {
+        moveDir = new Vector2(x, moveDir.y);
+    }
+
+    public void setMoveDir()
+    {
+
+    }
+
+
 }
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerCharacter))]
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] InputData inputData;
+
+    PlayerCharacter player;
+
+    void Awake()
+    {
+        player = GetComponent<PlayerCharacter>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +23,37 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(Input.GetKeyDown(inputData.up))
+        if(Input.GetKey(inputData.up))
+        {
+           player.setMoveY(1);
+           player.direction = "Up";
+        }
+        else if(Input.GetKey(inputData.down))
+        {
+            player.setMoveY(-1);
+            player.direction = "Down";
+        }
+        else 
+            player.setMoveY(0);
+            
 
-        // else(Input.GetKeyDown(inputData.down))
-        
-        // if(Input.GetKeyDown(inputData.left))
+        if(Input.GetKey(inputData.left))
+        {
+            player.setMoveX(-1);
+            player.direction = "Left";
+        }
+        else if(Input.GetKey(inputData.right))
+        {
+            player.setMoveX(1);
+            player.direction = "Right";
+        }
+        else 
+            player.setMoveX(0);
 
-        // else(Input.GetKeyDown(inputData.right))
+        if(player.moveDir == Vector2.zero)
+            player.action = "Idle";
+        else 
+            player.action = "Walk";
 
         
     }
